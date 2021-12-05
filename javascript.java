3a018653954d0a81b59fcb73cf517d6baaaf2902
“ElakkiyaVijayababu” <ennlucky@gmail.com>
2021-12-05 07:31:38 +0000

    master-firstcommit

---
+++ javascript.java
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+   <title>JavaScript Tutorial</title>
+</head>
+<body>
+
+<h2>JavaScript Tutorial - Learn JavaScript</h2>
+<p id="test">This is a simple first Example of JavaScript.</p>
+
+<button type="button"
+onclick="document.getElementById('test').innerHTML = 'Welcome to JavaScript!'">
+Click Me</button>
+
+</body>
+</html>
